97d9aad74d1a8e4079ef6e7827a88a6ba541515f
Ackrilll <skawlsgus2@naver.com>
2024-09-12 22:57:53 +0900

    [INIT]

---
+++ src/main/java/com/sparta/todojwt/TodoJwtApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoJwtApplication.class, args);
+    }
+
+}
